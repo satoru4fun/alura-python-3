2780bade46958b170139f0069c256a584ee3aa24
Guilherme Noguchi <8411808@educacao.intranet>
2019-05-10 09:02:58 -0300

    aula 5

--- advinhacao.py
+++ advinhacao.py
@@ -1,22 +1,34 @@
 print('Bem vindo ao jogo de advinhacao!')
 
 numero_secreto = 42
+total_tentativas = 3
+rodada = 1
 
-chute_str = input('Digite um numero: ')
+for rodada in range(1, total_tentativas + 1):
+    print('Tentativa {} de {}'.format(rodada, total_tentativas))
 
-print('Voce digitou: ', chute_str)
+    chute_str = input('Digite um numero entre 1 e 100: ')
 
-chute = int(chute_str)
+    print('Voce digitou: ', chute_str)
 
-acertou = chute == numero_secreto
-maior   = chute > numero_secreto
-menor   = chute < numero_secreto
+    chute = int(chute_str)
 
-if(acertou):
-    print('Voce acertou!')
-elif(maior):
-    print('Voce errou. O chute foi maior que o numero secreto')
-elif(menor):
-    print('Voce errou. O chute foi menor que o numero secreto')
+    numero_invalido = chute < 1 or chute > 100
 
+    if(numero_invalido):
+        print('Digite um numero entre 1 e 100!')
+        continue
+
+    acertou = chute == numero_secreto
+    maior   = chute > numero_secreto
+    menor   = chute < numero_secreto
+
+    if(acertou):
+        print('Voce acertou!')
+        break
+    elif(maior):
+        print('Voce errou. O chute foi maior que o numero secreto')
+    elif(menor):
+        print('Voce errou. O chute foi menor que o numero secreto')
+    
 print('Fim do jogo')
