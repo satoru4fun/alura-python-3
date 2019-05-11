c9b599b92dbc363974a3aee58726cdc405c8a885
Satoru Noguchi <satoru.nog@gmail.com>
2019-05-11 15:25:10 -0300

    aula 6

--- forca.py
+++ forca.py
@@ -2,22 +2,41 @@ def jogar():
 
     print('Bem vindo ao jogo de advinhacao!')
 
-    palavra_secreta = 'banana'
+    palavra_secreta = 'banana'.upper()
+
+    letras_acertadas = ['_' for letra in palavra_secreta]
+
+    print(letras_acertadas)
     
     enforcou = False
     ganhou   = False
+    erros = 0
 
     while(not enforcou and not ganhou):
         print('jogando...')
         chute = input('Qual a letra?')
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
         index = 0
 
-        for letra in palavra_secreta:
-            if(chute.upper()==letra.upper()):
-                print('A letra {} foi encontrado em {}'.format(chute, index))
-            index = index + 1
+        if(chute in palavra_secreta):
+            for letra in palavra_secreta:
+                if(chute.upper()==letra.upper()):
+                    letras_acertadas[index] = chute.lower()
+                index += 1
+        else:
+            erros += 1
+        
+        enforcou = erros >= 6
+
+        ganhou = '_' not in letras_acertadas
+
+        print(letras_acertadas)
+    
+    if(ganhou):
+        print('Voce ganhou!')
+    else:
+        print('Voce perdeu!')
 
     print('Fim do jogo')
 
